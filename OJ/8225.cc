452c49ffa85dc82cc87cbbea8152ed7291720249
EndstoneV <wdk2908786286@outlook.com>
2022-09-09 23:02:03 +0800

    8225

--- OJ/8225.cc
+++ OJ/8225.cc
@@ -3,34 +3,8 @@ using namespace std;
 #define ll long long
 #define ld double
 #define f(i, j, k) for(int i = j; i <= k; i++)
-int main()
+int main(int argc, char** argv)
 {
-    // int t;
-    // cin >> t;//有t组数据
-    // while(t--){
-    //     int k;
-    //     ll n;
-    //     cin >> k >> n;//每组数据的k, n
-    //     int cnt = 0;
-    //     while(n >= 1){
-    //         if(n % 10 != 9){//每一个数位均为9
-    //             cout << "baka" << endl;
-    //             goto end;
-    //         }
-    //         n /= 10;
-    //         cnt++;
-    //     }
-    //     if(k == 1){//满足以上条件都是1阶天才数
-    //         cout << "aya" << endl;
-    //         goto end;
-    //     }
-    //     if(cnt % k != 0){//不是k位
-    //         cout << "baka" << endl;
-    //         goto end;
-    //     }
-    //     cout << "aya" << endl;//否则满足
-    //     end:;
-    // }   
     int t;
     cin >> t;
     while(t--){
@@ -38,7 +12,6 @@ int main()
         char n[20];
         cin >> k;
         cin >> n + 1;
-        // cout << endl << n + 1 << endl;
         if(strlen(n) % k){
             cout << "baka" << endl;
             goto end;

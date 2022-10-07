3afe9cf6045ab1df1024ad8e75d1324b7d9448dc
EndstoneV <wdk2908786286@outlook.com>
2022-10-07 16:09:34 +0800

    NP

--- 10.5/ShanShu.cpp
+++ 10.5/ShanShu.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main() {
     int k;
     string s;
-    cin >> s;
-    scanf("%d", &k);
+    cin >> s >> k;
     while(k) {
         int i;
         for(i = 0; i < s.size() - 1 && s[i] <= s[i + 1]; i++)
@@ -16,7 +15,11 @@ int main() {
         cout << 0 << endl;
     }
     int i = 0;
-    for(i = 0; i < s.size() - 1, s[i] == '0'; i++)
-        ;
+    for(i = 0; i < s.size() - 1;) {
+        if(s[i] == '0')
+            i++;
+        else
+            break;
+    }
     cout << s.substr(i);
 }
--- 10.5/XiuLiNiuPeng.cpp
+++ 10.5/XiuLiNiuPeng.cpp
@@ -1,10 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define ll long long
-#define f(i, j, k) for(int i = j; i <= k; i++)
-
-int main(){
-    
-    
-    return 0;
+#define np next_permutation
+#define ld double
+using namespace  std;
+int a[203], b[203], c, m, s, len, j;
+int main() {
+    scanf("%d%d%d", &m, &s, &c);
+    for(int i = 0; i < c; i++)
+        scanf("%d", &a[i]);
+    sort(a, a + c);
+    for(int i = 1; i < c; i++)
+        b[i] = a[i] - a[i - 1] - 1;//number -> *|empty|*
+    sort(b + 1, b + c);
+    len = c; // chushi quanbu fugai (changdu )
+    j   = 1;
+    for(int x = c; x > m; x--, j++)//间隔小+数量一定=最节约
+        len += b[j];
+    printf("%d", len);
 }
